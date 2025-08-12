f5734d6691a216aaca61950d4e218671f4fd63c2
Ye Lin Aung <yelinaung2359@gmail.com>
2025-08-12 20:45:04 +0700

    feat: Initial project setup

---
+++ Nas/Presentation/MainScreen/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nas
+//
+//  Created by Ye Lin Aung on 12/08/2025.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
